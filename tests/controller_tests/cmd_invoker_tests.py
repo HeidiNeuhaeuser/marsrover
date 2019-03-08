4b2a68c915ed624ba3b6fd3efc420da63c103630
Heidi <h.neuhaeuser3@gmail.com>
2019-03-08 11:58:19 +0200

    added tests

---
+++ tests/controller_tests/cmd_invoker_tests.py
@@ -0,0 +1,7 @@
+import unittest
+
+
+class TestCommandInvoker(unittest.TestCase):
+    def test_initialize(self):
+        # TODO
+        self.assertEqual(1, 1)
---
+++ tests/controller_tests/command_tests.py
@@ -0,0 +1,7 @@
+import unittest
+
+
+class TestCommand(unittest.TestCase):
+    def test_initialize(self):
+        # TODO
+        self.assertEqual(1, 1)
