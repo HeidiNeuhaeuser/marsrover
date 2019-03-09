7e55ae970e72164969234f4951b0d77269e8aaef
Heidi <h.neuhaeuser3@gmail.com>
2019-03-09 17:06:33 +0200

    refactoring

--- marsrover/__init__.py
+++ marsrover/__init__.py
@@ -13,16 +13,14 @@ def parse_arguments():
 
     parser.add_argument(
         "-i",
-        "--input",
         help="Determine input file.",
-        required=False,
+        required=True,
         default="sample_input.txt",
         dest="input_file")
 
     parser.add_argument(
         "-o",
-        "--output",
-        help="Determine output file.",
+        help="Determine output file name.",
         required=False,
         default="sample_output.txt",
         dest="output_file")
@@ -37,36 +35,63 @@ def parse_arguments():
     return parser.parse_args()
 
 
-def main():
-    args = parse_arguments()
-
-    # configure logging
+def configure_logging(args_log_level):
     log_level = logging.INFO
-    if args.verbose:
+    if args_log_level:
         log_level = logging.DEBUG
+
     logging.basicConfig(
         stream=sys.stdout,
         level=log_level,
         format='[%(levelname)s]: (%(filename)s: line %(lineno)d) - %(message)s')
 
-    c = CommandParser(args.input_file)
 
+def check_input_file(i):
+    if not os.path.isfile(i):
+        exit_program("Input file does not exist in directory.")
+
+
+def write_output_file(file_name, content):
+    out = "marsrover_output.txt"
+    if file_name:
+        out = file_name
+
+    with open(out, 'w') as f:
+        f.write(content)
+    LOG.info("Output written into output file '{}'".format(out))
+
+
+def exit_program(reason):
+    LOG.error(reason)
+    LOG.error("Exit Program!")
+    exit(1)
+
+
+def get_rover_positions(cmd_invokers):
+    output_txt = ""
+    for rover_invoker in cmd_invokers:
+        rover, invoker = rover_invoker
+        invoker.execute_commands()
+        LOG.debug("Rover Position after commands: {}".format(str(rover)))
+        output_txt += "{} \n".format(str(rover))
+    return output_txt
+
+
+def main():
+    args = parse_arguments()
+
+    configure_logging(args.verbose)
+    check_input_file(args.input_file)
+
+    c = CommandParser(args.input_file)
     try:
         command_invokers = c.parse()
     except Exception:
-        LOG.error("Input file contains invalid input")
-        LOG.error("Exit program!")
-        traceback.print_exc()
-        exit(1)
+        exit_program("Input file contains invalid input.")
 
-    out = args.output_file
-    with open(out, 'w') as f:
-        for rover_invoker in command_invokers:
-            rover, invoker = rover_invoker
-            invoker.execute_commands()
-            LOG.info("Rover Position after commands: {}".format(str(rover)))
-            f.write("{}\n".format(str(rover)))
-        LOG.info("Output written into output file '{}'".format(out))
+    rover_positions_str = get_rover_positions(command_invokers)
+
+    write_output_file(args.output_file, rover_positions_str)
 
 
 if __name__ == "__main__":
